54a48a727a8c348f14f8832b262962305ca69e51
Precyzik <gawaprecyz@gmail.com>
2024-09-10 20:40:38 +0200

    More env vars.

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 ls -lah
 
-envsubst '$DB_USER,$DB_PASSWORD,$DATABASE' < backend.properties > liquibase.properties
+envsubst '$DB_USER,$DB_PASSWORD,$DB_URL,$DB_NAME' < backend.properties > liquibase.properties
 liquibase update
 
-envsubst '$DB_USER,$DB_PASSWORD' < carlydb.properties > liquibase.properties
+envsubst '$DB_USER,$DB_PASSWORD,$DB_URL' < carlydb.properties > liquibase.properties
 liquibase update
